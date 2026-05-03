984c88a127365064315cccdd8a2900feadabd7e7
Jin <Jin-23@mai.ru>
2026-05-03 12:02:49 +0300

    fix port and host

--- app.py
+++ app.py
@@ -152,5 +152,4 @@ async def webhook(req: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 5000))
-    uvicorn.run(app, host="::", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
