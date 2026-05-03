a8d1978aabfabad7a8010353c7fb0bc8f074352d
Jin <Jin-23@mai.ru>
2026-05-03 16:24:26 +0300

    universal version3

--- app.py
+++ app.py
@@ -110,7 +110,4 @@ async def webhook(req: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    import os
-    host = os.getenv("IP", "0.0.0.0")
-    port = int(os.getenv("PORT", "8000"))
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
